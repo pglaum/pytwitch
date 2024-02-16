0f03d90c58b548cd7bdd9cec0844467803f821e5
Philipp Glaum <philipp.glaum@sinetcon.com>
2024-02-16 20:24:34 +0100

    initial commit

---
+++ twitch/__init__.py
@@ -0,0 +1,55 @@
+from twitch.streams import Streams
+import requests
+
+
+class Twitch:
+
+    __base_url = "https://api.twitch.tv/helix/"
+    __debugging = False
+    __client_id = ""
+    __client_secret = ""
+    __access_token = ""
+
+    def __init__(self, client_id, client_secret):
+        self.__client_id = client_id
+        self.__client_secret = client_secret
+
+        self.streams = Streams(self)
+
+    def get(self, route, params, try_again=False):
+        url = self.__base_url + route
+        headers = {
+            "Client-ID": self.__client_id,
+            "Authorization": "Bearer " + self.__access_token,
+        }
+        res = requests.get(url, headers=headers, params=params)
+        if self.__debugging:
+            print(res.url)
+
+        if res.status_code == 401:
+            if not try_again:
+                self.get_access_token()
+                return self.get(route, params, try_again=True)
+
+            raise Exception("Unauthorized (already tried to get a new access token)")
+
+        return res.json()
+
+    def get_access_token(self):
+        url = "https://id.twitch.tv/oauth2/token"
+        params = {
+            "client_id": self.__client_id,
+            "client_secret": self.__client_secret,
+            "grant_type": "client_credentials",
+        }
+        res = requests.post(url, params=params)
+        self.__access_token = res.json()["access_token"]
+        return self.__access_token
+
+    def set_access_token(self, access_token):
+        self.__access_token = access_token
+        return self.__access_token
+
+    def set_debugging(self, debugging):
+        self.__debugging = debugging
+        return self.__debugging
---
+++ twitch/streams.py
@@ -0,0 +1,48 @@
+from twitch.models.pagination import Pagination
+from twitch.models.streams import Stream
+
+
+class Streams:
+    def __init__(self, twitch):
+        self.__twitch = twitch
+
+    def get_streams(
+        self,
+        after=None,
+        before=None,
+        limit=None,
+        game_id=None,
+        language=None,
+        user_id=None,
+        user_login=None,
+    ):
+        routes = "streams"
+        params = {}
+
+        if after is not None:
+            params["after"] = after
+        if before is not None:
+            params["before"] = before
+        if limit is not None:
+            params["limit"] = limit
+        if game_id is not None:
+            params["game_id"] = game_id
+        if language is not None:
+            params["language"] = language
+        if user_id is not None:
+            params["user_id"] = user_id
+        if user_login is not None:
+            params["user_login"] = user_login
+
+        res = self.__twitch.get(routes, params)
+
+        streams = []
+        if "data" in res:
+            for stream in res["data"]:
+                streams.append(Stream(**stream))
+
+        pagination = None
+        if "pagination" in res:
+            pagination = Pagination(**res["pagination"])
+
+        return streams, pagination
